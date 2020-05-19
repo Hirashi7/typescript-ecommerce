d959fbe7c4ecd1a610cfbe373309996f36c76b56
Grzegorz Wójcik <gwojcik@arkonsoft.pl>
2020-05-19 17:57:42 +0200

    konfiguracja projektu

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,22 @@
-import express = require('express');
+import * as dotenv from 'dotenv';
+import * as express from 'express';
+import * as mongoose from 'mongoose';
+import * as bodyParser from 'body-parser';
+
+dotenv.config();
+
+mongoose.connect(
+    'mongodb+srv://typeshopdb:' + process.env.DB_PASSWORD + '@typeshop-5u9aw.mongodb.net/test?retryWrites=true&w=majority',
+    { 
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    }
+);
 
 const app: express.Application = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hejka')
