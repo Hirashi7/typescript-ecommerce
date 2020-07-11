84ff64df3cf022a64a997b7a8439500f756a026b
Grzegorz Wójcik <gwojcik@arkonsoft.pl>
2020-07-11 12:40:52 +0200

    poprawki koncepcyjne

--- client/src/app/adapters/category.adapter.ts
+++ client/src/app/adapters/category.adapter.ts
@@ -7,4 +7,5 @@ export default interface CategoryAdapter {
     getApiModel(): CategoryApi;
 
     getClientModel(): Category;
+
 }
--- client/src/app/factories/category.creator.ts
+++ client/src/app/factories/category.creator.ts
@@ -1,6 +1,7 @@
 import Category from '../models/category.model';
 import CategoryApi from '../models/category.api.model';
+import { CategoryFactoryTypes } from './category.factory.types';
 
 export default abstract class CategoryCreator {
-    public abstract create(type, obj): Category | CategoryApi;
+    public abstract create(source: CategoryFactoryTypes, obj: any): Category | CategoryApi;
 }
